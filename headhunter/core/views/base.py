27d5c472d01ddcff9272e060a5dbc7a0a5f0560f
tanitaDev <tanita.pak@gmail.com>
2022-11-23 19:53:02 +0600

    #8 Основной функционал
    1. Добавила вывод вакансий по дате обновления
    2. Поменяла отображения карточек вакансий
    3. Добавила пагинацию

--- headhunter/core/views/base.py
+++ headhunter/core/views/base.py
@@ -6,19 +6,23 @@ from django.views.generic import ListView
 from accounts.forms import CustomUserCreationForm, LoginForm
 from accounts.models import Profile
 from core.models import Resume, Vacancy
+from django.core.paginator import Paginator
 
 
 class IndexView(ListView):
     template_name = 'index.html'
-    context_object_name = 'something'
-    model = Profile
+    paginate_by = 1
+    model = Vacancy
+    context_object_name = 'vacancy'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
         context['register_form'] = CustomUserCreationForm()
         context['login_form'] = LoginForm()
-        context['vacancies'] = Vacancy.objects.filter(is_active=False).order_by('-updated_at')
         return context
+
+    def get_queryset(self):
+        return Vacancy.objects.filter(is_active=False).order_by('-updated_at')
 
 
 def update_resume(request, *args, **kwargs):
