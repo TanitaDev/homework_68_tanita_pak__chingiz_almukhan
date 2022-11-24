e1aea3cbb11fd5a8846e7d24667b528764005de5
tanitaDev <tanita.pak@gmail.com>
2022-11-24 15:44:46 +0600

    #8 Основной функционал
    1. Добавила LiveSearch, без редактированния верстки

--- headhunter/core/views/api_views.py
+++ headhunter/core/views/api_views.py
@@ -1,5 +1,7 @@
 from django.core.serializers import serialize
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 from core.models import Vacancy
 
@@ -7,5 +9,5 @@ from core.models import Vacancy
 def vacancy_list_view(request, *args, **kwargs):
     if request.method == "GET":
         search = request.GET.get('search')
-        print(search)
-        return JsonResponse(serialize('json', Vacancy.objects.filter(name__icontains=search)), safe=False)
+        vacancy = Vacancy.objects.filter(name__icontains=search) if search else Vacancy.objects.all()
+        return JsonResponse(list(vacancy.values(*('name', 'salary', 'author'))), safe=False)
